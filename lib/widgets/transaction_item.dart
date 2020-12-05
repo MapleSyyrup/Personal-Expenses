c058cb80dee23acf139a5c1f529227cd62f8e2a1
MapleSyyrup <cha.cha.bumanlag@gmail.com>
2020-12-05 16:58:21 +0800

    Using keys - Value key is used to give a unique identifier in each transaction(widget).   Even if a transaction or a widget is removed. Flutter will be able to   identify which widget belongs to an element.

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -12,6 +14,26 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTx;
+
+  @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.purple,
+      Colors.pink,
+      Colors.blue,
+      Colors.green,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,25 +45,26 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('\$${transaction.amount}'),
+              child: Text('\$${widget.transaction.amount}'),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.subtitle2,
         ),
         subtitle: Text(
-          DateFormat.yMMMEd().format(transaction.date),
+          DateFormat.yMMMEd().format(widget.transaction.date),
         ),
         trailing: IconButton(
           icon: Icon(Icons.delete),
           color: Theme.of(context).errorColor,
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
         ),
       ),
     );
